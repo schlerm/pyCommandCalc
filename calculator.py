6caf068d26e0092374dd95d4ac1eeadc78bb3010
Stephen Schulte <sschulte@ucvts.org>
2019-02-13 11:56:12 -0500

    prints none for some reason

--- calculator.py
+++ calculator.py
@@ -1,1 +1,33 @@
-yeet
+def plus(x, y):
+    return x+y
+
+def minus(x, y):
+    return x-y
+
+def times(x, y):
+    return x*y
+
+def divide(x, y):
+    return x/y
+
+def calc():
+    operator = str(input("Select an operation (+,-,*, or /)"))
+    num1 = int(input("Enter the first number"))
+    num2 = int(input("Enter the second number"))
+    if operator == "+" :
+        print(num1,"+",num2,"=",plus(num1, num2))
+    elif operator == "-" :
+        print(num1,"-",num2,"=",minus(num1, num2))
+    elif operator == "*" :
+        print(num1,"*",num2,"=",times(num1, num2))
+    elif operator == "/" :
+        print(num1,"/",num2,"=",divide(num1, num2))
+    else:
+        print("This ain't it chief.")
+    cont = str(input(print("Continue? y or n")))
+    if cont == "y":
+        calc()
+
+calc()
+
+#prints none for some reason
